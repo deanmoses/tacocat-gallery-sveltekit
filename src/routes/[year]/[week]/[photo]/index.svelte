f10904c2e0f804c41e5e78e8ac8f33daf92cd2bc
Dean Moses <moses@tacocat.com>
2021-12-28 15:40:42 -0800

    Routing to photos correctly

---
+++ src/routes/[year]/[week]/[photo]/index.svelte
@@ -0,0 +1,44 @@
+<script context="module" lang="ts">
+	export async function load({ page }) {
+        const slug = page.params.photo;
+        return {props: {slug}}
+    }
+</script>
+
+<script lang="ts">
+    import Page from "../../../../lib/site/Page.svelte";
+    import Header from "../../../../lib/site/Header.svelte";
+    import Footer from "../../../../lib/site/Footer.svelte";
+    import Nav from "../../../../lib/site/Nav.svelte";
+    import PageContent from "../../../../lib/site/PageContent.svelte";
+    import MainContent from "../../../../lib/site/MainContent.svelte";
+    import PrevButton from "../../../../lib/site/PrevButton.svelte";
+    import UpButton from "../../../../lib/site/UpButton.svelte";
+    import NextButton from "../../../../lib/site/NextButton.svelte";
+
+    export let slug;
+</script>
+
+<svelte:head>
+	<title>{slug}</title>
+</svelte:head>
+
+<Page>
+    <Header>
+      <span slot="title">{slug}</span>
+    </Header>
+    <PageContent>
+      <MainContent>
+        <section>
+          <h2 style="display:none">Photo Description</h2>
+          No animals were harmed in the making of this photo
+        </section>
+        <Nav>
+            <PrevButton href="/2021/12-12/christmas" />
+            <UpButton title="December 20, 2021" href="/2021/12-20" />
+            <NextButton disabled />
+          </Nav>
+      </MainContent>
+    </PageContent>
+    <Footer />
+  </Page>
--- src/routes/[year]/[week]/index.svelte
+++ src/routes/[year]/[week]/index.svelte
@@ -7,17 +7,17 @@
 </script>
 
 <script lang="ts">
-    import Page from "../../lib/site/Page.svelte";
-    import Header from "../../lib/site/Header.svelte";
-    import Footer from "../../lib/site/Footer.svelte";
-    import Nav from "../../lib/site/Nav.svelte";
-    import PageContent from "../../lib/site/PageContent.svelte";
-    import MainContent from "../../lib/site/MainContent.svelte";
-    import Thumbnails from "../../lib/site/Thumbnails.svelte";
-    import Thumbnail from "../../lib/site/Thumbnail.svelte";
-    import PrevButton from "../../lib/site/PrevButton.svelte";
-    import UpButton from "../../lib/site/UpButton.svelte";
-    import NextButton from "../../lib/site/NextButton.svelte";
+    import Page from "../../../lib/site/Page.svelte";
+    import Header from "../../../lib/site/Header.svelte";
+    import Footer from "../../../lib/site/Footer.svelte";
+    import Nav from "../../../lib/site/Nav.svelte";
+    import PageContent from "../../../lib/site/PageContent.svelte";
+    import MainContent from "../../../lib/site/MainContent.svelte";
+    import Thumbnails from "../../../lib/site/Thumbnails.svelte";
+    import Thumbnail from "../../../lib/site/Thumbnail.svelte";
+    import PrevButton from "../../../lib/site/PrevButton.svelte";
+    import UpButton from "../../../lib/site/UpButton.svelte";
+    import NextButton from "../../../lib/site/NextButton.svelte";
 
     export let week;
     export let year;
@@ -32,8 +32,8 @@
       <span slot="title">{week}-{year}</span>
     </Header>
     <Nav>
-      <PrevButton title="2020" href="#2020" />
-      <UpButton title="All Years" href="#" />
+      <PrevButton title="2020" href="/2021/12-12" />
+      <UpButton title="All Years" href="/" />
       <NextButton disabled />
     </Nav>
     <PageContent>
@@ -44,18 +44,18 @@
         </section>
         <Thumbnails>
           <Thumbnail
-            title="Dec 20"
-            href="#2021/12-20"
+            title="Brothers"
+            href="/2021/12-20/brothers"
             src="https://cdn.tacocat.com/zenphoto/cache/2021/12-20/xmas_restaurant2_200_w200_h200_cw200_ch200_thumb.jpg?cached=1640062642"
           />
           <Thumbnail
-            title="Dec 20"
-            href="#2021/12-20"
+            title="Fancy Dinner"
+            href="/2021/12-20/fancy_dinner"
             src="https://cdn.tacocat.com/zenphoto/cache/2021/12-20/xmas_restaurant2_200_w200_h200_cw200_ch200_thumb.jpg?cached=1640062642"
           />
           <Thumbnail
-            title="Dec 20"
-            href="#2021/12-20"
+            title="Christmas"
+            href="/2021/12-20/christmas"
             src="https://cdn.tacocat.com/zenphoto/cache/2021/12-20/xmas_restaurant2_200_w200_h200_cw200_ch200_thumb.jpg?cached=1640062642"
           />
         </Thumbnails>
