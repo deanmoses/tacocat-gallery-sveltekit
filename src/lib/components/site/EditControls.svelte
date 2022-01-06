aa66458729b0c143e48e51b08435718ce12af00d
Dean Moses <moses@tacocat.com>
2022-01-05 19:49:06 -0800

    Code cleanup

--- src/lib/components/site/EditControls.svelte
+++ src/lib/components/site/EditControls.svelte
@@ -7,55 +7,60 @@
 <script lang="ts">
   import { editMode } from "$lib/stores/EditModeStore";
 
-  function onEditClick() {
+  function onEditButtonClick() {
     const isEditing = true;
     editMode.set(isEditing);
   }
-  function onCancelClick() {
+
+  function onCancelButtonClick() {
     const isEditing = false;
     editMode.set(isEditing);
   }
+
+	function onSaveButtonClick() {
+		console.log("TODO: Save button clicked");
+	}
 </script>
 
-<style>
-    .edit-controls {
-        position: fixed;
-        top: 0;
-        left: 0;
-        min-height: 4em;
-        min-width: 4em;
-    }
-
-    .edit-controls button {
-        margin: 0.7em;
-        display: none; /* will be overridden on hover */
-    }
-
-    .edit-controls:hover button {
-        display: inherit;/* */
-    }
-
-    .editing-controls {
-        display: flex;
-        justify-content: flex-end;
-        gap: .5em;
-        padding: .5em;
-        background-color: rgb(65, 64, 64);
-        border-bottom: 1px solid black;
-    }
-
-    .cancel {
-      margin-right: auto;
-    }
-</style>
-
 {#if !$editMode}
-  <div class="edit-controls">
-    <button on:click|once={onEditClick}>Edit</button>
+  <div class="not-yet-editing-controls">
+    <button on:click|once={onEditButtonClick}>Edit</button>
   </div>
 {:else}
   <div class="editing-controls">
-    <button class="cancel" on:click|once={onCancelClick}>Cancel</button> 
-    <button class="save">Save</button>
+    <button class="cancel" on:click|once={onCancelButtonClick}>Cancel</button> 
+    <button class="save" on:click|once={onSaveButtonClick}>Save</button>
   </div>
 {/if}
+
+<style>
+	.not-yet-editing-controls {
+			position: fixed;
+			top: 0;
+			left: 0;
+			min-height: 4em;
+			min-width: 4em;
+	}
+
+	.not-yet-editing-controls button {
+			margin: 0.7em;
+			display: none; /* will be overridden on hover */
+	}
+
+	.not-yet-editing-controls:hover button {
+			display: inherit;/* */
+	}
+
+	.editing-controls {
+			display: flex;
+			justify-content: flex-end;
+			gap: .5em;
+			padding: .5em;
+			background-color: rgb(65, 64, 64);
+			border-bottom: 1px solid black;
+	}
+
+	.editing-controls .cancel {
+		margin-right: auto;
+	}
+</style>
