9e88ea7d040f52f133b75bda15a14e4cc2f2d6eb
Dean Moses <moses@tacocat.com>
2022-01-07 10:49:02 -0800

    Bug in blank image page

--- src/lib/components/pages/image/layouts/BlankImagePageLayout.svelte
+++ src/lib/components/pages/image/layouts/BlankImagePageLayout.svelte
@@ -7,7 +7,6 @@
 
 <script lang="ts">
 	import ImagePageLayout from "./ImagePageLayout.svelte";
-	import Header from "$lib/components/site/Header.svelte";
 	import PrevButton from "$lib/components/site/nav/PrevButton.svelte";
 	import UpButton from "$lib/components/site/nav/UpButton.svelte";
 	import NextButton from "$lib/components/site/nav/NextButton.svelte";
@@ -18,17 +17,21 @@
 </script>
 
 <ImagePageLayout {year} {title}>
-	<svelte:fragment slot="header">
-		<Header />
+
+	<svelte:fragment slot="title">
+		{title}
 	</svelte:fragment>
+
 	<svelte:fragment slot="nav">
 		<PrevButton />
 		<UpButton />
 		<NextButton />
 	</svelte:fragment>
+
 	<svelte:fragment slot="image">
 		<FullPageMessage>
 			<slot />
 		</FullPageMessage>
 	</svelte:fragment>
+	
 </ImagePageLayout>
