935b718602d2353a0a14c6affb02dcef38967f00
Dean Moses <moses@tacocat.com>
2022-01-06 22:24:57 -0800

    Show edit toggle on image page

--- src/lib/components/pages/image/ImagePage.svelte
+++ src/lib/components/pages/image/ImagePage.svelte
@@ -6,6 +6,7 @@
 
 <script lang="ts">
 	import ImagePageLayout from "./layouts/ImagePageLayout.svelte";
+	import EditToggle from "$lib/components/site/editControls/EditToggle.svelte";
 	import Header from "$lib/components/site/Header.svelte";
 	import PrevButton from "$lib/components/site/nav/PrevButton.svelte";
 	import UpButton from "$lib/components/site/nav/UpButton.svelte";
@@ -19,18 +20,27 @@
 </script>
 
 <ImagePageLayout {title} {year}>
+
+	<svelte:fragment slot="editControls">
+		<EditToggle />
+	</svelte:fragment>
+
 	<svelte:fragment slot="header">
 		<Header title="{image.title}" hideSiteTitle/>
 	</svelte:fragment>
+
 	<svelte:fragment slot="caption">
 		<EditableHtml htmlContent={image.desc}/>
 	</svelte:fragment>
+
 	<svelte:fragment slot="nav">
 		<PrevButton href={image.prevImageHref} />
 		<UpButton href={$album.href} title={$album.pageTitle}/>
 		<NextButton href={image.nextImageHref} />
 	</svelte:fragment>
+
 	<svelte:fragment slot="image">
 		<a href="https://tacocat.com{image.url_full}" target="zen"><img src="https://cdn.tacocat.com{image.url_sized}" style="object-fit: contain; width: 100%; height: 100%; max-width: 4032px; max-height: 3024px;" alt="{image.title}"></a>
 	</svelte:fragment>
+	
 </ImagePageLayout>
