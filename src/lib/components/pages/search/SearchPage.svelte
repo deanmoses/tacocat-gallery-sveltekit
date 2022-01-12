d7f60b0ad3c571b0e5ea92abc3edbacf015e6c2d
Dean Moses <moses@tacocat.com>
2022-01-12 12:12:02 -0800

    Refactored the "go back" icon into its own component

--- src/lib/components/pages/search/SearchPage.svelte
+++ src/lib/components/pages/search/SearchPage.svelte
@@ -7,6 +7,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import SiteLayout from '$lib/components/site/SiteLayout.svelte';
+	import ReturnIcon from '$lib/components/site/icons/ReturnIcon.svelte';
 
   export let searchTerms: string = null;
 	export let returnPath: string = null;
@@ -29,7 +30,7 @@
 <SiteLayout hideFooter>
 	<header>
 		<a href={returnPath}>
-			<svg style="width: 1em; height: 1em;" viewBox="0 0 1156 1156"><path style="fill: currentcolor;" d="M578 0q118 0 225 45.5t184.5 123 123 184.5 45.5 225-45.5 225-123 184.5-184.5 123-225 45.5-225-45.5-184.5-123T45.5 803 0 578t45.5-225 123-184.5T353 45.5 578 0zm-39 323L264 546q-16 13-16 32t16 32l275 223q16 13 27.5 8t11.5-26V678h275q10 0 17.5-7.5T878 653V503q0-10-7.5-17.5T853 478H578V341q0-21-11.5-26t-27.5 8z"></path></svg>
+			<ReturnIcon />
 		</a>
 		<form on:submit|preventDefault={onSubmit}>
 			<input
@@ -58,10 +59,10 @@
 		background-color: var(--header-color);
 	}
   
-	svg {
+	a {
+		color: var(--default-text-color);
 		font-size: 28px;
 		min-height: 1.5em; /* for when there's no h1 text */
-		color: var(--default-text-color);
 	}
 
 	form {
