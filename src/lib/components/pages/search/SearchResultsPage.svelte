5253d385e5df7c258012d791b84a730df5340c3c
Dean Moses <moses@tacocat.com>
2022-01-13 14:33:57 -0800

    Display empty search results nicer

--- src/lib/components/pages/search/SearchResultsPage.svelte
+++ src/lib/components/pages/search/SearchResultsPage.svelte
@@ -9,14 +9,19 @@
 	import Thumbnails from "$lib/components/site/Thumbnails.svelte";
 	import SearchPage from "$lib/components/pages/search/SearchPage.svelte";
 	import Config from "$lib/utils/config";
+	import type { SearchResults } from "$lib/models/search";
+import FullPageMessage from "$lib/components/site/FullPageMessage.svelte";
 
 	export let searchTerms: string;
-	export let searchResults;
+	export let searchResults: SearchResults;
 	export let returnPath: string;
+
+	let noResults: boolean;
+	$: noResults = !searchResults.images && !searchResults.albums;
 </script>
 
 <SearchPage {searchTerms} {returnPath}>
-	<section>
+	<section class:noResults={noResults}>
 		<h2 style="display:none">Search Results</h2>
 		<Thumbnails>
 			{#if searchResults.albums}
@@ -40,6 +45,12 @@
 				/>
 			{/each}
 			{/if}
+
+			{#if !searchResults.images && !searchResults.albums}
+				<FullPageMessage>
+					No results
+				</FullPageMessage>
+			{/if}
 		</Thumbnails>
 	</section>
 </SearchPage>
@@ -48,5 +59,10 @@
 	section {
 		background-color: white;
 		padding: var(--default-padding);
+	}
+
+	section.noResults {
+		display:flex;
+		justify-content: center;
 	}
 </style>
