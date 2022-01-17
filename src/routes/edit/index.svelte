1939067a511fc2406e5c097eea77ac2ae9ea35a4
Dean Moses <moses@tacocat.com>
2022-01-16 22:58:16 -0800

    Prettier display of error

--- src/routes/edit/index.svelte
+++ src/routes/edit/index.svelte
@@ -1,2 +1,8 @@
-<p>Nothing on the root album is editable</p>
-<p>How did you even GET here?</p>
+<script lang="ts">
+	import AlbumErrorPage from "$lib/components/pages/album/AlbumErrorPage.svelte";
+</script>
+
+<AlbumErrorPage>
+	<p>Nothing on the root album is editable</p>
+	<p>How did you even GET here?</p>
+</AlbumErrorPage>
