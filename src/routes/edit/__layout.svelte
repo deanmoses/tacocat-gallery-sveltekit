100f46d58c4dd8c2919759bd64705831a9ab6528
Dean Moses <moses@tacocat.com>
2022-01-18 21:23:50 -0800

    Protect edit pages: redirect unauthenticated people to the non-edit version of the page
    This is so if Lucie or I send a URL to the edit version of tacocat, people will get redirected.

--- src/routes/edit/__layout.svelte
+++ src/routes/edit/__layout.svelte
@@ -1,12 +1,26 @@
-<script>
-	import AlbumErrorPage from "$lib/components/pages/album/AlbumErrorPage.svelte";
-	import { isAdmin } from "$lib/stores/SessionStore";
+<script context="module" lang="ts">
+  import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+	import { unEditUrl } from '$lib/utils/path-utils';
+
+	/** @type {import('@sveltejs/kit').Load} */
+  export async function load({ url }: LoadInput): Promise<LoadOutput> {
+		// if user isn't authenticated
+		if (!get(isAdmin)) {
+			// redirect them to the non-edit version of this page
+			return {
+				status: 302,
+				redirect: unEditUrl(url.pathname)
+			};
+		}
+		else {
+			return {};
+		}
+  }
 </script>
 
-{#if $isAdmin}
-	<slot />
-{:else}
-	<AlbumErrorPage>
-		You aren't authorized to access edit functionality
-	</AlbumErrorPage>
-{/if}
+<script>
+	import { isAdmin } from "$lib/stores/SessionStore";
+	import { get } from 'svelte/store';
+</script>
+
+<slot />
