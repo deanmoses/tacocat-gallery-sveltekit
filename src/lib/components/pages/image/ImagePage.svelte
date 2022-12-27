b6e6d3d8e2c0f119b7a06b0adda38c2c8db084b1
Dean Moses <moses@tacocat.com>
2022-12-26 18:18:11 -0800

    Reformatting

--- src/lib/components/pages/image/ImagePage.svelte
+++ src/lib/components/pages/image/ImagePage.svelte
@@ -3,16 +3,14 @@
 
   Page to display an image
 -->
-
 <script lang="ts">
-	import ImagePageLayout from "./layouts/ImagePageLayout.svelte";
-	import PrevButton from "$lib/components/site/nav/PrevButton.svelte";
-	import UpButton from "$lib/components/site/nav/UpButton.svelte";
-	import NextButton from "$lib/components/site/nav/NextButton.svelte";
-	import BigImage from "./BigImage.svelte";
-	import EditToggle from "$lib/components/site/edit/toggle/EditToggle.svelte";
-	import type { Album, Image } from "$lib/models/album";
-	import Config from "$lib/utils/config";
+	import ImagePageLayout from './layouts/ImagePageLayout.svelte';
+	import PrevButton from '$lib/components/site/nav/PrevButton.svelte';
+	import UpButton from '$lib/components/site/nav/UpButton.svelte';
+	import NextButton from '$lib/components/site/nav/NextButton.svelte';
+	import BigImage from './BigImage.svelte';
+	import EditToggle from '$lib/components/site/edit/toggle/EditToggle.svelte';
+	import type { Album, Image } from '$lib/models/album';
 
 	export let year: string;
 	export let album: Album;
@@ -20,7 +18,6 @@
 </script>
 
 <ImagePageLayout {year} title={image.title}>
-
 	<svelte:fragment slot="editControls">
 		<EditToggle />
 	</svelte:fragment>
@@ -37,12 +34,11 @@
 
 	<svelte:fragment slot="nav">
 		<PrevButton href={image.prevImageHref} />
-		<UpButton href={album.href} title={album.pageTitle}/>
+		<UpButton href={album.href} title={album.pageTitle} />
 		<NextButton href={image.nextImageHref} />
 	</svelte:fragment>
 
 	<svelte:fragment slot="image">
 		<BigImage {image} />
 	</svelte:fragment>
-
 </ImagePageLayout>
