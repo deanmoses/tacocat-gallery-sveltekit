e14e07f4112397ebe02e34e6c168c1028de12c83
Dean Moses <moses@tacocat.com>
2022-12-26 19:59:36 -0800

    Reformatted comment

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,5 @@
-// Disable server side rendering (SSR) globally, for every page of the entire app
-// In other words, this turns the app into a Single Page Application (SPA)
+/**
+ * Disable server side rendering (SSR) globally, for every page of the entire app
+ * In other words, this turns the app into a Single Page Application (SPA)
+ */
 export const ssr = false;
