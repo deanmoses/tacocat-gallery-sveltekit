cc690a24fb06a0ce8bc92204b90382ea7658986e
Dean Moses <moses@tacocat.com>
2022-12-30 11:54:25 -0800

    Refactored legacy URL redirect functionality out to its own function

---
+++ src/lib/utils/legacyUrlHandler.ts
@@ -0,0 +1,18 @@
+import { redirect } from '@sveltejs/kit';
+
+/**
+ * If passed in a legacy hashtag-based URL like '#2001/12-31/felix.jpg', send
+ * a Sveltekit redirect to the correct URL.  If passed in an empty / null
+ * string, do nothing.
+ *
+ * @param hashLocation a legacy hashtag-based URL, like: #2001/12-31/felix.jpg
+ */
+export function redirectIfLegacyUrl(hashLocation: string) {
+	if (hashLocation) {
+		let newUrl = hashLocation.substring(1); // remove the '#'
+		if (!newUrl.startsWith('/')) {
+			newUrl = '/' + newUrl;
+		}
+		throw redirect(301, newUrl);
+	}
+}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,15 +1,10 @@
 import type { PageLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
 import { albumStore } from '$lib/stores/AlbumStore';
+import { redirectIfLegacyUrl } from '$lib/utils/legacyUrlHandler';
 
 export const load: PageLoad = () => {
-	// If the URL contains an old style album in the hash, like #2001/12-31/felix.jpg
-	if (location.hash) {
-		const newUrl = location.hash.substring(1);
-		throw redirect(301, newUrl);
-	}
+	redirectIfLegacyUrl(location.hash);
 
-	// Otherwise show root album
 	const pathToRootAlbum = '';
 	return {
 		albumEntry: albumStore.get(pathToRootAlbum)
