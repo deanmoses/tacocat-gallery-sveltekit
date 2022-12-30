50807e76ee06d38cf36fa0a1f8527bb725717fdf
Dean Moses <moses@tacocat.com>
2022-12-30 12:36:20 -0800

    Automatic reformatting

--- src/lib/components/pages/album/day/DayAlbumPage.svelte
+++ src/lib/components/pages/album/day/DayAlbumPage.svelte
@@ -3,23 +3,21 @@
 
   Page showing a day album
 -->
-
 <script lang="ts">
-	import DayAlbumPageLayout from "./DayAlbumPageLayout.svelte";
-	import EditToggle from "$lib/components/site/edit/toggle/EditToggle.svelte";
-	import PrevButton from "$lib/components/site/nav/PrevButton.svelte";
-	import UpButton from "$lib/components/site/nav/UpButton.svelte";
-	import NextButton from "$lib/components/site/nav/NextButton.svelte";
-	import Thumbnail from "$lib/components/site/Thumbnail.svelte";
-	import Config from "$lib/utils/config";
-	import type { Album } from "$lib/models/album";
+	import DayAlbumPageLayout from './DayAlbumPageLayout.svelte';
+	import EditToggle from '$lib/components/site/edit/toggle/EditToggle.svelte';
+	import PrevButton from '$lib/components/site/nav/PrevButton.svelte';
+	import UpButton from '$lib/components/site/nav/UpButton.svelte';
+	import NextButton from '$lib/components/site/nav/NextButton.svelte';
+	import Thumbnail from '$lib/components/site/Thumbnail.svelte';
+	import Config from '$lib/utils/config';
+	import type { Album } from '$lib/models/album';
 
 	export let year: string;
 	export let album: Album;
 </script>
 
 <DayAlbumPageLayout {year} title={album.pageTitle}>
-
 	<svelte:fragment slot="editControls">
 		<EditToggle />
 	</svelte:fragment>
@@ -29,18 +27,9 @@
 	</svelte:fragment>
 
 	<svelte:fragment slot="nav">
-		<PrevButton
-			href={album.nextAlbumHref}
-			title={album.nextAlbumTitle}
-		/>
-		<UpButton
-			href={album.parentAlbumHref}
-			title={album.parentAlbumTitle}
-		/>
-		<NextButton 
-			href={album.prevAlbumHref}
-			title={album.prevAlbumTitle}
-		/>
+		<PrevButton href={album.nextAlbumHref} title={album.nextAlbumTitle} />
+		<UpButton href={album.parentAlbumHref} title={album.parentAlbumTitle} />
+		<NextButton href={album.prevAlbumHref} title={album.prevAlbumTitle} />
 	</svelte:fragment>
 
 	<svelte:fragment slot="caption">
@@ -51,15 +40,14 @@
 
 	<svelte:fragment slot="thumbnails">
 		{#if album.images}
-		{#each album.images as image (image.path)}
-			<Thumbnail
-				title={image.title}
-				summary={image.customdata}
-				href="/{image.path}"
-				src={Config.cdnUrl(image.url_thumb)}
-			/>
-		{/each}
+			{#each album.images as image (image.path)}
+				<Thumbnail
+					title={image.title}
+					summary={image.customdata}
+					href="/{image.path}"
+					src={Config.cdnUrl(image.url_thumb)}
+				/>
+			{/each}
 		{/if}
 	</svelte:fragment>
-
 </DayAlbumPageLayout>
