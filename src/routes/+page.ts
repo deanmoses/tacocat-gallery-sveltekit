c8e7b4841bdeabedc3c56a30edc4e69724e4a66c
Dean Moses <moses@tacocat.com>
2022-12-29 20:51:30 -0800

    Handle the old hash-based URL format: #2001/12-31/foo.jpg

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,16 @@
 import type { PageLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 import { albumStore } from '$lib/stores/AlbumStore';
 
 export const load: PageLoad = () => {
-	const pathToRootAlbum = '';
+	// If the URL contains an old style album in the hash, like #2001/12-31/felix.jpg
+	if (location.hash) {
+		const newUrl = location.hash.substring(1);
+		throw redirect(301, newUrl);
+	}
 
+	// Otherwise show root album
+	const pathToRootAlbum = '';
 	return {
 		albumEntry: albumStore.get(pathToRootAlbum)
 	};
