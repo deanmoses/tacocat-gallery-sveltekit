dc791344d16c8c2f1127ce83ed85f29abc279fdd
Dean Moses <moses@tacocat.com>
2022-12-30 15:25:38 -0800

    Hangover from migration to Sveltekit 1.0

--- src/routes/edit/+layout.ts
+++ src/routes/edit/+layout.ts
@@ -1,19 +1,17 @@
-import type { PageLoad } from './$types';
+import type { LayoutLoad } from './$types';
 import { unEditUrl } from '$lib/utils/path-utils';
-
-import { isAdmin } from "$lib/stores/SessionStore";
+import { isAdmin } from '$lib/stores/SessionStore';
 import { get } from 'svelte/store';
 
-export const load: PageLoad = ({ url }) => {
-    // if user isn't authenticated
-    if (!get(isAdmin)) {
-        // redirect them to the non-edit version of this page
-        return {
-            status: 302,
-            redirect: unEditUrl(url.pathname)
-        };
-    }
-    else {
-        return {};
-    }
-}
+export const load: LayoutLoad = ({ url }) => {
+	// if user isn't authenticated
+	if (!get(isAdmin)) {
+		// redirect them to the non-edit version of this page
+		return {
+			status: 302,
+			redirect: unEditUrl(url.pathname)
+		};
+	} else {
+		return {};
+	}
+};
