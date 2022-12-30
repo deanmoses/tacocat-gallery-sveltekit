3c1c06a7874e6271e459e702c9b6f8f9d0dffae7
Dean Moses <moses@tacocat.com>
2022-12-30 12:37:10 -0800

    Fix more strict TypeScript warnings

--- src/lib/models/impl/album-day.ts
+++ src/lib/models/impl/album-day.ts
@@ -5,11 +5,10 @@ import { longDate } from '$lib/utils/date-utils';
  * Overrides the default album class with  behavior specific to year albums.
  */
 export default class DayAlbum extends DateBasedAlbum {
-	
 	/**
 	 * Friendly title of page
 	 */
 	get pageTitle(): string {
-		return longDate(this.date);
+		return this.date ? longDate(this.date) : '';
 	}
 }
