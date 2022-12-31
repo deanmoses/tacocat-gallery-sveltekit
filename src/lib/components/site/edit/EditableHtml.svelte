962d4cebc3726aa884ac5b503e91d1a16fd4e01e
Dean Moses <moses@tacocat.com>
2022-12-30 17:26:55 -0800

    Automatic formatting

--- src/lib/components/site/edit/EditableHtml.svelte
+++ src/lib/components/site/edit/EditableHtml.svelte
@@ -5,26 +5,25 @@
 	When you select some text, it pops up a rich text editor
 -->
 <script lang="ts">
-	import HtmlEditor from "./HtmlEditor.svelte";
-	import DraftStore from "$lib/stores/DraftStore";
+	import HtmlEditor from './HtmlEditor.svelte';
+	import DraftStore from '$lib/stores/DraftStore';
 
-	/** 
-	 * The HTML content to be made editable 
+	/**
+	 * The HTML content to be made editable
 	 */
-	export let htmlContent = "";
+	export let htmlContent = '';
 
 	// The edited content
 	let newHtmlContent: string;
 
 	// Update the draft store any time the HTML is edited
 	// The $: is Svelte syntax.  This gets compiled into a
-	// reactive statement that executes whenever any of the 
+	// reactive statement that executes whenever any of the
 	// variables referenced within it changes.
 	$: {
 		if (newHtmlContent == null) {
-			console.log("<EditableHtml>: newHtmlContent null");
-		}
-		else {
+			console.log('<EditableHtml>: newHtmlContent null');
+		} else {
 			DraftStore.setDesc(newHtmlContent);
 		}
 	}
