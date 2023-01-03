b964439a3ce3715b6e74088c9adcb3d447f6a331
Dean Moses <moses@tacocat.com>
2023-01-02 16:29:46 -0800

    Ignore a11y warnings around managing the selected thumbnail

--- src/lib/components/site/icons/Icon.svelte
+++ src/lib/components/site/icons/Icon.svelte
@@ -3,18 +3,18 @@
 
   Base component for all SVG icons
 -->
-
 <script lang="ts">
-	export let width = "1em";
-	export let height = "1em";
-	export let title: string = null;
+	export let width = '1em';
+	export let height = '1em';
+	export let title: string | undefined = undefined;
 	export let d: string;
 	export let viewBox: string;
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <svg on:click {width} {height} {viewBox}>
 	{#if title}<title>{title}</title>{/if}
-	<path {d}></path>
+	<path {d} />
 </svg>
 
 <style>
