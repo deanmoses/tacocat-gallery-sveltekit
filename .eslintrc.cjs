78e73590b3571a84426f98f91822125f7da5cd8a
Dean Moses <moses@tacocat.com>
2023-11-10 15:34:03 -0500

    More strict Typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,10 @@ module.exports = {
         sourceType: 'module',
         ecmaVersion: 2020,
     },
+    extends: [
+        'plugin:@typescript-eslint/recommended', // recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
     env: {
         browser: true,
         es2017: true,
