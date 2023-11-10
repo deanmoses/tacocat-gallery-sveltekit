fc5e398a7a9fcd14287b79a83b75932e878a5bb9
Dean Moses <moses@tacocat.com>
2023-11-10 14:54:54 -0500

    Prettier

--- src/routes/[year=year]/+page.svelte
+++ src/routes/[year=year]/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import YearAlbumRouting from '$lib/components/pages/album/year/YearAlbumRouting.svelte';
-	import YearAlbumPage from '$lib/components/pages/album/year/YearAlbumPage.svelte';
-	import type { PageData } from './$types';
+    import YearAlbumRouting from '$lib/components/pages/album/year/YearAlbumRouting.svelte';
+    import YearAlbumPage from '$lib/components/pages/album/year/YearAlbumPage.svelte';
+    import type { PageData } from './$types';
 
-	export let data: PageData;
+    export let data: PageData;
 
-	$: year = data.year;
-	$: albumEntry = data.albumEntry;
-	$: album = $albumEntry.album;
-	$: status = $albumEntry.loadStatus;
+    $: year = data.year;
+    $: albumEntry = data.albumEntry;
+    $: album = $albumEntry.album;
+    $: status = $albumEntry.loadStatus;
 </script>
 
 <YearAlbumRouting {status} {year}>
-	<svelte:fragment slot="loaded">
-		<YearAlbumPage {album} {year} />
-	</svelte:fragment>
+    <svelte:fragment slot="loaded">
+        <YearAlbumPage {album} {year} />
+    </svelte:fragment>
 </YearAlbumRouting>
--- src/routes/edit/[year=year]/[day=day]/+page.svelte
+++ src/routes/edit/[year=year]/[day=day]/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import DayAlbumRouting from '$lib/components/pages/album/day/DayAlbumRouting.svelte';
-	import DayAlbumEditPage from '$lib/components/pages/album/day/DayAlbumEditPage.svelte';
-	import type { PageData } from './$types';
+    import DayAlbumRouting from '$lib/components/pages/album/day/DayAlbumRouting.svelte';
+    import DayAlbumEditPage from '$lib/components/pages/album/day/DayAlbumEditPage.svelte';
+    import type { PageData } from './$types';
 
-	export let data: PageData;
+    export let data: PageData;
 
-	$: year = data.year;
-	$: albumEntry = data.albumEntry;
-	$: album = $albumEntry.album;
-	$: status = $albumEntry.loadStatus;
+    $: year = data.year;
+    $: albumEntry = data.albumEntry;
+    $: album = $albumEntry.album;
+    $: status = $albumEntry.loadStatus;
 </script>
 
 <DayAlbumRouting {status} {year}>
-	<svelte:fragment slot="loaded">
-		<DayAlbumEditPage {album} {year} />
-	</svelte:fragment>
+    <svelte:fragment slot="loaded">
+        <DayAlbumEditPage {album} {year} />
+    </svelte:fragment>
 </DayAlbumRouting>
