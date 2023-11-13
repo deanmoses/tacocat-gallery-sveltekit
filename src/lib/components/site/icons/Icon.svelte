707e8b3b776c045d57c702bf04cc483a3e7326d3
Dean Moses <moses@tacocat.com>
2023-11-13 16:12:51 -0500

    Prettier + ignoring an a11y warning

--- src/lib/components/site/icons/Icon.svelte
+++ src/lib/components/site/icons/Icon.svelte
@@ -4,27 +4,28 @@
   Base component for all SVG icons
 -->
 <script lang="ts">
-	export let width = '1em';
-	export let height = '1em';
-	export let title: string | undefined = undefined;
-	export let d: string;
-	export let viewBox: string;
+    export let width = '1em';
+    export let height = '1em';
+    export let title: string | undefined = undefined;
+    export let d: string;
+    export let viewBox: string;
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <svg on:click {width} {height} {viewBox}>
-	{#if title}<title>{title}</title>{/if}
-	<path {d} />
+    {#if title}<title>{title}</title>{/if}
+    <path {d} />
 </svg>
 
 <style>
-	svg {
-		display: inline;
-		position: relative;
-		bottom: 0.125em;
-		vertical-align: middle;
-	}
-	path {
-		fill: currentcolor;
-	}
+    svg {
+        display: inline;
+        position: relative;
+        bottom: 0.125em;
+        vertical-align: middle;
+    }
+    path {
+        fill: currentcolor;
+    }
 </style>
