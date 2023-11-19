828ad8c9aa7f54843dd81e160131e8b4ffb02afe
Dean Moses <moses@tacocat.com>
2023-11-19 14:31:26 -0800

    Shrunk icon to match the other icons

--- src/lib/components/site/icons/EditIcon.svelte
+++ src/lib/components/site/icons/EditIcon.svelte
@@ -8,7 +8,7 @@
 
     const d =
         'M1027 0q19 0 33 13l153 153q13 14 13 33t-13 33l-99 92-214-214 95-96q13-14 32-14zm2 410l-615 614-214-214 614-614zm-696 704L0 1226l110-335z';
-    const viewBox = '0 0 1226 1226';
+    const viewBox = '0 -320 1600 1600';
 </script>
 
 <Icon on:click {viewBox} {title} {d} />
