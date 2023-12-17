6e1a3b9d9891b2a411e80ce1c3779d6ec1c31187
Dean Moses <moses@tacocat.com>
2023-12-17 14:11:26 -0800

    Remove support for legacy URL format
    This fixes #52

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,7 @@
 import type { PageLoad } from './$types';
 import { albumStore } from '$lib/stores/AlbumStore';
-import { redirectIfLegacyUrl } from '$lib/utils/legacyUrlHandler';
 
 export const load: PageLoad = () => {
-    redirectIfLegacyUrl(location.hash);
     const pathToRootAlbum = '/';
     return {
         albumEntry: albumStore.get(pathToRootAlbum),
