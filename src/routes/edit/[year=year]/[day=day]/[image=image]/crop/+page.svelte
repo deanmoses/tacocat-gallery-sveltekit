d17d6274e466daf72efc11cd45b829ef1e5ca130
Dean Moses <moses@tacocat.com>
2025-02-02 22:35:32 -0800

    More moving albums from Svelte stores to runes

--- src/routes/edit/[year=year]/[day=day]/[image=image]/crop/+page.svelte
+++ src/routes/edit/[year=year]/[day=day]/[image=image]/crop/+page.svelte
@@ -2,16 +2,18 @@
     import type { PageProps } from './$types';
     import CropPage from '$lib/components/pages/image/CropPage.svelte';
     import ImageRouting from '$lib/components/pages/image/ImageRouting.svelte';
+    import { albumStore } from '$lib/stores/AlbumStore.svelte';
 
     let { data }: PageProps = $props();
-    let albumEntry = $derived(data.albumEntry);
-    let album = $derived($albumEntry.album);
-    let albumLoadStatus = $derived($albumEntry.loadStatus);
+    let albumPath = $derived(data.albumPath);
+    let albumEntry = $derived(albumStore.albums.get(albumPath));
+    let album = $derived(albumEntry?.album);
+    let albumLoadStatus = $derived(albumEntry?.loadStatus);
     let imagePath = $derived(data.imagePath);
     let image = $derived(album?.getImage(imagePath));
 </script>
 
-<ImageRouting {albumLoadStatus} {image}>
+<ImageRouting {image} {albumLoadStatus}>
     {#snippet loaded()}
         {#if image}
             <CropPage {image} />
--- src/routes/edit/[year=year]/[day=day]/[image=image]/crop/+page.ts
+++ src/routes/edit/[year=year]/[day=day]/[image=image]/crop/+page.ts
@@ -4,10 +4,9 @@ import { albumStore } from '$lib/stores/AlbumStore.svelte';
 export const load: PageLoad = ({ params }) => {
     const albumPath = `/${params.year}/${params.day}/`;
     const imagePath = `${albumPath}${params.image}`;
-    const albumEntry = albumStore.get(albumPath);
-
+    albumStore.fetch(albumPath);
     return {
+        albumPath,
         imagePath,
-        albumEntry,
     };
 };
